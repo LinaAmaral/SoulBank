71281e15d4d336ee9fe6e43bb5459332a92dfb1e
LinaAmaral <lina.amaralcaetano@gmail.com>
2021-09-26 13:12:59 -0300

    ajuste dos caminhos das views

--- src/routers/contaRouters.ts
+++ src/routers/contaRouters.ts
@@ -4,12 +4,20 @@ import * as controlador from "../controllers/contaController"
 // const controller = controlador;
 export const router = express.Router();
 
-router.get('/ola', (request, response) => {
-  response.send('Hello world!');
-});
-
 router.get('/', (request, response) => {
   response.render("login");
+});
+
+router.get('/transacoes', (request, response) => {
+  response.render("transacoes");
+});
+
+router.get('/transferencia', (request, response) => {
+  response.render("transferencia");
+});
+
+router.get('/saldo', (request, response) => {
+  response.render("saldo");
 });
 
 
